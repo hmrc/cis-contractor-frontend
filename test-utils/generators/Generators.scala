1858fa48296eecdf28f81891c28717547f545ccc
edpau-hmrc <232416446+edpau-hmrc@users.noreply.github.com>
2025-12-09 12:54:06 +0000

    DTR-1873: add unit test for SubUseTradingNamePage

--- test-utils/generators/Generators.scala
+++ test-utils/generators/Generators.scala
@@ -22,7 +22,7 @@ import org.scalacheck.Arbitrary._
 import org.scalacheck.Gen._
 import org.scalacheck.{Gen, Shrink}
 
-trait Generators extends ModelGenerators {
+trait Generators extends UserAnswersGenerator with ModelGenerators with PageGenerators with UserAnswersEntryGenerators {
 
   implicit val dontShrink: Shrink[String] = Shrink.shrinkAny
 
