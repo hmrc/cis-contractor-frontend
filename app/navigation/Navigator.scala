5f7b169194cb9b2e2824322b86fceb86a9fa2c5b
javed Iqbal <131012491+javed-iqbal1@users.noreply.github.com>
2025-12-12 09:58:23 +0000

    DTR-1877 update navigator

--- app/navigation/Navigator.scala
+++ app/navigation/Navigator.scala
@@ -21,7 +21,7 @@ import play.api.mvc.Call
 import controllers.routes
 import pages.*
 import models.*
-import pages.add.{TradingNameOfSubcontractorPage, TypeOfSubcontractorPage}
+import pages.add.{AddressOfSubcontractorPage, TradingNameOfSubcontractorPage, TypeOfSubcontractorPage}
 
 @Singleton
 class Navigator @Inject() () {
@@ -29,6 +29,7 @@ class Navigator @Inject() () {
   private val normalRoutes: Page => UserAnswers => Call = {
     case TypeOfSubcontractorPage => _ => controllers.add.routes.TypeOfSubcontractorController.onPageLoad(NormalMode)
     case TradingNameOfSubcontractorPage => _ => controllers.add.routes.TradingNameOfSubcontractorController.onPageLoad(NormalMode)
+    case AddressOfSubcontractorPage => _ => controllers.add.routes.AddressOfSubcontractorController.onPageLoad(NormalMode)
     case _                      => _ => routes.IndexController.onPageLoad()
   }
 
