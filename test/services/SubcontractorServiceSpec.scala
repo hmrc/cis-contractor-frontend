55835c77db8757419d5d1f71175db55d4322d9d8
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2026-01-13 09:58:28 +0000

    DTR-1778: service tests updated

--- test/services/SubcontractorServiceSpec.scala
+++ test/services/SubcontractorServiceSpec.scala
@@ -19,6 +19,7 @@ package services
 import base.SpecBase
 import connectors.ConstructionIndustrySchemeConnector
 import models.add.{SubcontractorName, TypeOfSubcontractor}
+import models.response.CisTaxpayerResponse
 import models.subcontractor.{CreateSubcontractorRequest, CreateSubcontractorResponse, UpdateSubcontractorRequest, UpdateSubcontractorResponse}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.{verify, verifyNoInteractions, verifyNoMoreInteractions, when}
@@ -36,6 +37,146 @@ final class SubcontractorServiceSpec extends SpecBase with MockitoSugar {
   implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.global
 
   "SubcontractorService" - {
+
+    "initializeCisId" - {
+      "should initialize CisId if it is not in session data" in {
+        val mockConnector: ConstructionIndustrySchemeConnector = mock[ConstructionIndustrySchemeConnector]
+        val service                                            = new SubcontractorService(mockConnector)
+
+        val cisId       = "cisId"
+        val userAnswers = emptyUserAnswers
+
+        when(mockConnector.getCisTaxpayer()(any[HeaderCarrier]))
+          .thenReturn(
+            Future.successful(
+              CisTaxpayerResponse(
+                uniqueId = cisId,
+                taxOfficeNumber = "taxOfficeNumber",
+                taxOfficeRef = "taxOfficeRef",
+                aoDistrict = None,
+                aoPayType = None,
+                aoCheckCode = None,
+                aoReference = None,
+                validBusinessAddr = None,
+                correlation = None,
+                ggAgentId = None,
+                employerName1 = None,
+                employerName2 = None,
+                agentOwnRef = None,
+                schemeName = None,
+                utr = None,
+                enrolledSig = None
+              )
+            )
+          )
+
+        val result = service.initializeCisId(userAnswers)
+
+        val expectedUserAnswers = userAnswers
+          .set(CisIdQuery, cisId)
+          .success
+          .value
+
+        result.futureValue mustBe expectedUserAnswers
+
+        verify(mockConnector).getCisTaxpayer()(any[HeaderCarrier])
+        verifyNoMoreInteractions(mockConnector)
+      }
+
+      "should return user answers when CisIs is already in session data " in {
+        val mockConnector: ConstructionIndustrySchemeConnector = mock[ConstructionIndustrySchemeConnector]
+        val service                                            = new SubcontractorService(mockConnector)
+
+        val cisId               = "cisId"
+        val expectedUserAnswers = emptyUserAnswers
+          .set(CisIdQuery, cisId)
+          .success
+          .value
+
+        when(mockConnector.getCisTaxpayer()(any[HeaderCarrier]))
+          .thenReturn(
+            Future.successful(
+              CisTaxpayerResponse(
+                uniqueId = cisId,
+                taxOfficeNumber = "taxOfficeNumber",
+                taxOfficeRef = "taxOfficeRef",
+                aoDistrict = None,
+                aoPayType = None,
+                aoCheckCode = None,
+                aoReference = None,
+                validBusinessAddr = None,
+                correlation = None,
+                ggAgentId = None,
+                employerName1 = None,
+                employerName2 = None,
+                agentOwnRef = None,
+                schemeName = None,
+                utr = None,
+                enrolledSig = None
+              )
+            )
+          )
+
+        val result = service.initializeCisId(expectedUserAnswers)
+
+        result.futureValue mustBe expectedUserAnswers
+
+        verifyNoInteractions(mockConnector)
+      }
+
+      "should fail when connector returns empty CisId when CisId not in session data" in {
+        val mockConnector: ConstructionIndustrySchemeConnector = mock[ConstructionIndustrySchemeConnector]
+        val service                                            = new SubcontractorService(mockConnector)
+
+        val cisId       = ""
+
+        when(mockConnector.getCisTaxpayer()(any[HeaderCarrier]))
+          .thenReturn(
+            Future.successful(
+              CisTaxpayerResponse(
+                uniqueId = cisId,
+                taxOfficeNumber = "taxOfficeNumber",
+                taxOfficeRef = "taxOfficeRef",
+                aoDistrict = None,
+                aoPayType = None,
+                aoCheckCode = None,
+                aoReference = None,
+                validBusinessAddr = None,
+                correlation = None,
+                ggAgentId = None,
+                employerName1 = None,
+                employerName2 = None,
+                agentOwnRef = None,
+                schemeName = None,
+                utr = None,
+                enrolledSig = None
+              )
+            )
+          )
+
+        val exception =
+          service.initializeCisId(emptyUserAnswers).failed.futureValue
+
+        exception.getMessage must include("Empty cisId (uniqueId) returned from /cis/taxpayer")
+      }
+
+      "should fail when the connector call fails" in {
+        val mockConnector: ConstructionIndustrySchemeConnector = mock[ConstructionIndustrySchemeConnector]
+        val service                                            = new SubcontractorService(mockConnector)
+
+        when(mockConnector.getCisTaxpayer()(any[HeaderCarrier]))
+          .thenReturn(Future.failed(new Exception("bang")))
+
+        val exception =
+          service.initializeCisId(emptyUserAnswers).failed.futureValue
+
+        exception.getMessage must include("bang")
+
+        verify(mockConnector).getCisTaxpayer()(any[HeaderCarrier])
+        verifyNoMoreInteractions(mockConnector)
+      }
+    }
+
     "createSubcontractor" - {
       "should create a subcontractor when a subcontractor type is provided" in {
         val mockConnector: ConstructionIndustrySchemeConnector = mock[ConstructionIndustrySchemeConnector]
