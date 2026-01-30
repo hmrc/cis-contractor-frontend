d3ac243554bcec1dd66952bb832540ebf627ec92
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2026-01-30 14:47:20 +0000

    DTR-2969: scaffolding

---
+++ test/forms/PartnershipContactDetailsYesNoFormProviderSpec.scala
@@ -0,0 +1,29 @@
+package forms
+
+import forms.behaviours.BooleanFieldBehaviours
+import play.api.data.FormError
+
+class PartnershipContactDetailsYesNoFormProviderSpec extends BooleanFieldBehaviours {
+
+  val requiredKey = "partnershipContactDetailsYesNo.error.required"
+  val invalidKey = "error.boolean"
+
+  val form = new PartnershipContactDetailsYesNoFormProvider()()
+
+  ".value" - {
+
+    val fieldName = "value"
+
+    behave like booleanField(
+      form,
+      fieldName,
+      invalidError = FormError(fieldName, invalidKey)
+    )
+
+    behave like mandatoryField(
+      form,
+      fieldName,
+      requiredError = FormError(fieldName, requiredKey)
+    )
+  }
+}
