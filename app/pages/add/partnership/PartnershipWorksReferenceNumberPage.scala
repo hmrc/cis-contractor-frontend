c0698a9734ba38945b4d79979b3c8e2e45839eea
Juely Kaikade <254691220+Juely-Kaikade-HMRC@users.noreply.github.com>
2026-01-30 14:42:33 +0000

    DTR 2623 -Screen SL0205-I (PTN) What is the works reference number of [partnership name]? (#33)
    * DTR-2623: Screen SL0205-I (PTN) What is the works reference number of [partnership name]?
    
    * DTR-2623 - all review comments have been addressed
    
    * DTR-2623 - Title updated for partnership works reference

--- app/pages/add/partnership/PartnershipWorksReferenceNumberPage.scala
+++ app/pages/add/partnership/PartnershipWorksReferenceNumberPage.scala
@@ -19,9 +19,9 @@ package pages.add.partnership
 import pages.QuestionPage
 import play.api.libs.json.JsPath
 
-case object PartnershipNamePage extends QuestionPage[String] {
+case object PartnershipWorksReferenceNumberPage extends QuestionPage[String] {
 
   override def path: JsPath = JsPath \ toString
 
-  override def toString: String = "partnershipName"
+  override def toString: String = "partnershipWorksReference"
 }
