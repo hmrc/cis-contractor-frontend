017bcb934f89bc1816fbf803031f851cf2cd3290
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2026-02-02 13:43:37 +0000

    DTR-2969: duplicate test corrected

--- test/controllers/add/partnership/PartnershipContactDetailsYesNoControllerSpec.scala
+++ test/controllers/add/partnership/PartnershipContactDetailsYesNoControllerSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.add.partnership
 
 import base.SpecBase
@@ -130,20 +146,6 @@ class PartnershipContactDetailsYesNoControllerSpec extends SpecBase with Mockito
           request,
           messages(application)
         ).toString
-      }
-    }
-
-    "must redirect to Journey Recovery for a GET if no existing data is found" in {
-
-      val application = applicationBuilder(userAnswers = None).build()
-
-      running(application) {
-        val request = FakeRequest(GET, partnershipContactDetailsYesNoRoute)
-
-        val result = route(application, request).value
-
-        status(result) mustEqual SEE_OTHER
-        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
       }
     }
 
