7ae3300676eaed92883eacb56069e9c13dd413a8
szaniszlo <15050363+szaniszlo@users.noreply.github.com>
2026-02-16 11:39:00 +0000

    DTR-2933 added CYA Summary pages and addressed PR review comments

---
+++ test/viewmodels/checkAnswers/add/partnership/PartnershipUniqueTaxpayerReferenceSummarySpec.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels.checkAnswers.add.partnership
+
+import controllers.add.partnership.routes
+import models.{CheckMode, UserAnswers}
+import org.scalatest.OptionValues.convertOptionToValuable
+import org.scalatest.TryValues.convertTryToSuccessOrFailure
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+import pages.add.partnership.PartnershipUniqueTaxpayerReferencePage
+import play.api.i18n.Messages
+import play.api.test.Helpers.stubMessages
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.*
+
+class PartnershipUniqueTaxpayerReferenceSummarySpec extends AnyFreeSpec with Matchers {
+
+  implicit val messages: Messages = stubMessages()
+
+  "PartnershipUniqueTaxpayerReferenceSummary.row" - {
+
+    "must return a SummaryListRow when the answer exists" in {
+      val answers =
+        UserAnswers("test-id")
+          .set(PartnershipUniqueTaxpayerReferencePage, "1234567890")
+          .success
+          .value
+
+      val maybeRow = PartnershipUniqueTaxpayerReferenceSummary.row(answers)
+      maybeRow shouldBe defined
+
+      val row = maybeRow.value
+
+      val expectedKeyText = messages("partnershipUniqueTaxpayerReference.checkYourAnswersLabel")
+      row.key.content.asHtml.toString should include(expectedKeyText)
+
+      row.value.content.asHtml.toString should include("1234567890")
+
+      row.actions shouldBe defined
+      val actions = row.actions.value.items
+      actions should have size 1
+
+      val changeAction       = actions.head
+      val expectedChangeText = messages("site.change")
+      val expectedHref       = routes.PartnershipUniqueTaxpayerReferenceController.onPageLoad(CheckMode).url
+      val expectedHiddenText = messages("partnershipUniqueTaxpayerReference.change.hidden")
+
+      changeAction.content.asHtml.toString    should include(expectedChangeText)
+      changeAction.href                     shouldBe expectedHref
+      changeAction.visuallyHiddenText.value shouldBe expectedHiddenText
+    }
+
+    "must return None when the answer does not exist" in {
+      val answers = UserAnswers("test-id")
+      PartnershipUniqueTaxpayerReferenceSummary.row(answers) shouldBe None
+    }
+  }
+}
