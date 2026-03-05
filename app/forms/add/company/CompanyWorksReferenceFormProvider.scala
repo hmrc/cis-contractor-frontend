139de25c790dc1f1afd9a5836ebe792e6e7e1be9
sonalgupta1227 <sonal.ab.gupta@digital.hmrc.gov.uk>
2026-03-05 11:02:38 +0000

    DTR-3152: controller and view changes

--- app/forms/add/company/CompanyWorksReferenceFormProvider.scala
+++ app/forms/add/company/CompanyWorksReferenceFormProvider.scala
@@ -16,6 +16,8 @@
 
 package forms.add.company
 
+import forms.Validation
+import forms.mappings.Constants.MaxLength20
 import forms.mappings.Mappings
 import play.api.data.Form
 
@@ -26,6 +28,11 @@ class CompanyWorksReferenceFormProvider @Inject() extends Mappings {
   def apply(): Form[String] =
     Form(
       "value" -> text("companyWorksReference.error.required")
-        .verifying(maxLength(100, "companyWorksReference.error.length"))
+        .verifying(
+          firstError(
+            maxLength(MaxLength20, "companyWorksReference.error.length"),
+            regexp(Validation.worksRefRegex, "companyWorksReference.error.invalid")
+          )
+        )
     )
 }
