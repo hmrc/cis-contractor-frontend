edddbbdcf8156a9bcf9cfa878e0c0b642c5a07d7
Richy Jassal <20478717+jassalrichy@users.noreply.github.com>
2026-04-01 08:04:14 +0100

    [DTR-4176] Resole pr comments

---
+++ test/models/agent/AgentClientIdentifiersSpec.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.agent
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+
+class AgentClientIdentifiersSpec extends AnyFreeSpec with Matchers {
+
+  "AgentClientIdentifiers" - {
+
+    "must hold the provided values" in {
+      val model = AgentClientIdentifiers(
+        uniqueId = "unique-id-123",
+        taxOfficeNumber = "123",
+        taxOfficeReference = "AB12345"
+      )
+
+      model.uniqueId mustBe "unique-id-123"
+      model.taxOfficeNumber mustBe "123"
+      model.taxOfficeReference mustBe "AB12345"
+    }
+
+    "must support equality based on its fields" in {
+      val a = AgentClientIdentifiers("id", "ton", "tor")
+      val b = AgentClientIdentifiers("id", "ton", "tor")
+      val c = AgentClientIdentifiers("different", "ton", "tor")
+
+      a mustEqual b
+      a must not equal c
+    }
+  }
+}
