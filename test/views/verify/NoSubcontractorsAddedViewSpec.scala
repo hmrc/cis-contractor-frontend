dfa35b7dc677ae443d49cf3d79fa98d6dadae779
mounkumar <49392940+Mounkumar@users.noreply.github.com>
2026-04-16 15:18:20 +0100

    DTR-4531 Removed the Setup trait and inlined shared fixtures

--- test/views/verify/NoSubcontractorsAddedViewSpec.scala
+++ test/views/verify/NoSubcontractorsAddedViewSpec.scala
@@ -28,52 +28,64 @@ import views.html.verify.NoSubcontractorsAddedView
 
 class NoSubcontractorsAddedViewSpec extends AnyWordSpec with Matchers with GuiceOneAppPerSuite {
 
+  implicit val request: Request[_] =
+    FakeRequest()
+
+  implicit val messages: Messages =
+    MessagesImpl(
+      Lang.defaultLang,
+      app.injector.instanceOf[MessagesApi]
+    )
+
+  implicit val appConfig: FrontendAppConfig =
+    app.injector.instanceOf[FrontendAppConfig]
+
+  val view: NoSubcontractorsAddedView =
+    app.injector.instanceOf[NoSubcontractorsAddedView]
+
   "NoSubcontractorsAddedView" should {
 
-    "render the page with correct title, heading, first paragraph and both links" in new Setup {
+    "render the page with correct title, heading, first paragraph and both links" in {
 
       val addSubcontractorsUrl = "/add-subcontractors"
 
-      private val html = view(addSubcontractorsUrl)
-      private val doc  = Jsoup.parse(html.toString())
+      val html = view(addSubcontractorsUrl)
+      val doc  = Jsoup.parse(html.toString())
 
-      doc.select("title").text() must include(messages("verify.noSubcontractorsAdded.title"))
-
-      doc.select("h1").size() mustBe 1
-      doc.select("h1").text() mustBe messages("verify.noSubcontractorsAdded.heading")
-
-      doc.select("p.govuk-body").text() must include(messages("verify.noSubcontractorsAdded.p1"))
-
-      private val addLink = doc.select(s"a[href='$addSubcontractorsUrl']")
-      addLink.size() mustBe 1
-      addLink.text() mustBe messages("verify.noSubcontractorsAdded.p2.link")
-
-      addLink.first().parent().text() must include(messages("verify.noSubcontractorsAdded.p2"))
-      addLink.first().parent().text() must include(messages("verify.noSubcontractorsAdded.p2.end"))
-
-      private val manageLink = doc.select(s"a[href='${appConfig.manageSubcontractorsUrl}']")
-      manageLink.size() mustBe 1
-      manageLink.text() mustBe messages("verify.noSubcontractorsAdded.p3.link")
-
-      manageLink.first().parent().text() must include(messages("verify.noSubcontractorsAdded.p3"))
-    }
-  }
-
-  trait Setup {
-
-    implicit val request: Request[_] =
-      FakeRequest()
-
-    implicit val messages: Messages =
-      MessagesImpl(
-        Lang.defaultLang,
-        app.injector.instanceOf[MessagesApi]
+      doc.select("title").text() must include(
+        messages("verify.noSubcontractorsAdded.title")
       )
 
-    implicit val appConfig: FrontendAppConfig =
-      app.injector.instanceOf[FrontendAppConfig]
+      doc.select("h1").size() mustBe 1
+      doc.select("h1").text() mustBe
+        messages("verify.noSubcontractorsAdded.heading")
 
-    val view: NoSubcontractorsAddedView =
-      app.injector.instanceOf[NoSubcontractorsAddedView]
+      doc.select("p.govuk-body").text() must include(
+        messages("verify.noSubcontractorsAdded.p1")
+      )
+
+      val addLink =
+        doc.select(s"a[href='$addSubcontractorsUrl']")
+      addLink.size() mustBe 1
+      addLink.text() mustBe
+        messages("verify.noSubcontractorsAdded.p2.link")
+
+      addLink.first().parent().text() must include(
+        messages("verify.noSubcontractorsAdded.p2")
+      )
+      addLink.first().parent().text() must include(
+        messages("verify.noSubcontractorsAdded.p2.end")
+      )
+
+      val manageLink =
+        doc.select(s"a[href='${appConfig.manageSubcontractorsUrl}']")
+      manageLink.size() mustBe 1
+      manageLink.text() mustBe
+        messages("verify.noSubcontractorsAdded.p3.link")
+
+      manageLink.first().parent().text() must include(
+        messages("verify.noSubcontractorsAdded.p3")
+      )
+    }
   }
 }
