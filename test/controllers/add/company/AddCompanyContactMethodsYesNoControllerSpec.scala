6f77ae6d8126670f761506510faeaf4ac2b78a80
edpau-hmrc <232416446+edpau-hmrc@users.noreply.github.com>
2026-06-12 10:07:15 +0100

    DTR-6013: Screen [Company] AS-Contact-C Do you want to add contact details?

---
+++ test/controllers/add/company/AddCompanyContactMethodsYesNoControllerSpec.scala
@@ -0,0 +1,259 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.add.company
+
+import base.SpecBase
+import controllers.routes
+import forms.add.company.AddCompanyContactMethodsYesNoFormProvider
+import models.{NormalMode, UserAnswers}
+import navigation.{FakeNavigator, Navigator}
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar
+import pages.add.company.{AddCompanyContactMethodsYesNoPage, CompanyNamePage}
+import play.api.inject.bind
+import play.api.mvc.Call
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import repositories.SessionRepository
+import views.html.add.company.AddCompanyContactMethodsYesNoView
+
+import scala.concurrent.Future
+
+class AddCompanyContactMethodsYesNoControllerSpec extends SpecBase with MockitoSugar {
+
+  private val formProvider = new AddCompanyContactMethodsYesNoFormProvider()
+  private val form         = formProvider()
+
+  private lazy val addCompanyContactMethodsYesNoRoute =
+    controllers.add.company.routes.AddCompanyContactMethodsYesNoController.onPageLoad(NormalMode).url
+
+  private val companyName = "Test Company"
+
+  private def uaWithName: UserAnswers =
+    emptyUserAnswers.set(CompanyNamePage, companyName).success.value
+
+  "AddCompanyContactMethodsYesNo Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(uaWithName)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, addCompanyContactMethodsYesNoRoute)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[AddCompanyContactMethodsYesNoView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form, NormalMode, companyName)(request, messages(application)).toString
+      }
+    }
+
+    "must populate the view correctly on a GET when the question has previously been answered" in {
+
+      val userAnswers = uaWithName.set(AddCompanyContactMethodsYesNoPage, true).success.value
+
+      val application = applicationBuilder(userAnswers = Some(userAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, addCompanyContactMethodsYesNoRoute)
+
+        val view = application.injector.instanceOf[AddCompanyContactMethodsYesNoView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(form.fill(true), NormalMode, companyName)(
+          request,
+          messages(application)
+        ).toString
+      }
+    }
+
+    "must redirect to the AddCompanyContactMethodsYesNo page when valid data with value Yes is submitted" in {
+
+      val mockSessionRepository = mock[SessionRepository]
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val application =
+        applicationBuilder(userAnswers = Some(uaWithName))
+          .overrides(
+            bind[SessionRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, addCompanyContactMethodsYesNoRoute)
+            .withFormUrlEncodedBody(("value", "true"))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual controllers.add.company.routes.AddCompanyContactMethodsYesNoController
+          .onPageLoad(NormalMode)
+          .url
+      }
+    }
+
+    "must redirect to the CompanyUtrYesNo page when valid data with value No is submitted" in {
+
+      val mockSessionRepository = mock[SessionRepository]
+
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val application =
+        applicationBuilder(userAnswers = Some(uaWithName))
+          .overrides(
+            bind[SessionRepository].toInstance(mockSessionRepository)
+          )
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, addCompanyContactMethodsYesNoRoute)
+            .withFormUrlEncodedBody(("value", "false"))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual controllers.add.company.routes.CompanyUtrYesNoController
+          .onPageLoad(NormalMode)
+          .url
+      }
+    }
+
+    "must return a Bad Request and errors when invalid data is submitted (name present)" in {
+
+      val application = applicationBuilder(userAnswers = Some(uaWithName)).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, addCompanyContactMethodsYesNoRoute)
+            .withFormUrlEncodedBody(("value", ""))
+
+        val boundForm = form.bind(Map("value" -> ""))
+
+        val view = application.injector.instanceOf[AddCompanyContactMethodsYesNoView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+        contentAsString(result) mustEqual view(boundForm, NormalMode, companyName)(
+          request,
+          messages(application)
+        ).toString
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request = FakeRequest(GET, addCompanyContactMethodsYesNoRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must redirect to Journey Recovery for a POST if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, addCompanyContactMethodsYesNoRoute)
+            .withFormUrlEncodedBody(("value", "true"))
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must return a Bad Request and errors when no value is submitted" in {
+
+      val application = applicationBuilder(userAnswers = Some(uaWithName)).build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, addCompanyContactMethodsYesNoRoute)
+            .withFormUrlEncodedBody()
+
+        val form      = new AddCompanyContactMethodsYesNoFormProvider()()
+        val boundForm = form.bind(Map.empty)
+
+        val view = application.injector.instanceOf[AddCompanyContactMethodsYesNoView]
+
+        val result = route(application, request).value
+
+        status(result) mustEqual BAD_REQUEST
+        contentAsString(result) mustEqual view(boundForm, NormalMode, companyName)(
+          request,
+          messages(application)
+        ).toString
+
+        contentAsString(result) must include(messages(application)("addCompanyContactMethodsYesNo.error.required"))
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET when subcontractor name is missing (userAnswers present)" in {
+
+      val application =
+        applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, addCompanyContactMethodsYesNoRoute)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+
+    "must redirect to Journey Recovery for a POST when subcontractor name is missing (userAnswers present)" in {
+
+      val mockSessionRepository = mock[SessionRepository]
+      when(mockSessionRepository.set(any())) thenReturn Future.successful(true)
+
+      val application =
+        applicationBuilder(userAnswers = Some(emptyUserAnswers))
+          .overrides(bind[SessionRepository].toInstance(mockSessionRepository))
+          .build()
+
+      running(application) {
+        val request =
+          FakeRequest(POST, addCompanyContactMethodsYesNoRoute)
+            .withFormUrlEncodedBody("value" -> "true")
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+  }
+}
---
+++ test/views/add/company/AddCompanyContactMethodsYesNoViewSpec.scala
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.add.company
+
+import forms.add.company.AddCompanyContactMethodsYesNoFormProvider
+import models.NormalMode
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.data.Form
+import play.api.i18n.Messages
+import play.api.mvc.Request
+import play.api.test.FakeRequest
+import play.twirl.api.HtmlFormat
+import views.html.add.company.AddCompanyContactMethodsYesNoView
+
+class AddCompanyContactMethodsYesNoViewSpec extends AnyWordSpec with Matchers with GuiceOneAppPerSuite {
+  "AddCompanyContactMethodsYesNoView" should {
+
+    "render the page with title, heading, radios and submit button" in new Setup {
+
+      val partnershipName = "Test Subcontractor"
+
+      val html: HtmlFormat.Appendable = view(form, NormalMode, partnershipName)
+      val doc: Document               = org.jsoup.Jsoup.parse(html.toString())
+      doc.select("title").text() must include(messages("addCompanyContactMethodsYesNo.title"))
+
+      val legend: Elements = doc.select("fieldset legend")
+      legend.text() mustBe messages("addCompanyContactMethodsYesNo.heading", partnershipName)
+      legend.hasClass("govuk-fieldset__legend--l") mustBe true
+
+      val radioButtons: Elements = doc.select(".govuk-radios__label")
+      radioButtons.size() mustBe 2
+      radioButtons.get(0).text mustBe "Yes"
+      radioButtons.get(1).text mustBe "No"
+
+      doc
+        .select("form")
+        .attr("action") mustBe controllers.add.company.routes.AddCompanyContactMethodsYesNoController
+        .onSubmit(NormalMode)
+        .url
+
+      doc.select("form").attr("autocomplete") mustBe "off"
+
+      doc.select(".govuk-button").text() mustBe messages("site.continue")
+    }
+
+    "display error summary and inline error when no option is selected" in new Setup {
+
+      val partnershipName = "Test Subcontractor"
+
+      val errorForm: Form[Boolean] =
+        form.withError("value", "addCompanyContactMethodsYesNo.error.required")
+
+      val html: HtmlFormat.Appendable = view(errorForm, NormalMode, partnershipName)
+      val doc: Document               = org.jsoup.Jsoup.parse(html.toString())
+
+      val summary: Elements = doc.select(".govuk-error-summary")
+      summary.text() must include(messages("addCompanyContactMethodsYesNo.error.required"))
+
+      val linkHref: String = summary.select("a").attr("href")
+      linkHref mustBe "#value_0"
+
+      doc.select(".govuk-error-message").text() must include(
+        messages("addCompanyContactMethodsYesNo.error.required")
+      )
+    }
+  }
+
+  trait Setup {
+    val formProvider        = new AddCompanyContactMethodsYesNoFormProvider()
+    val form: Form[Boolean] = formProvider()
+
+    implicit val request: Request[_] = FakeRequest()
+    implicit val messages: Messages  =
+      play.api.i18n.MessagesImpl(
+        play.api.i18n.Lang.defaultLang,
+        app.injector.instanceOf[play.api.i18n.MessagesApi]
+      )
+
+    val view: AddCompanyContactMethodsYesNoView = app.injector.instanceOf[AddCompanyContactMethodsYesNoView]
+  }
+
+}
