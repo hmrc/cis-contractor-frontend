43d90aac3750f444b1c7a4010f98c1f328c58f75
jowensoc <114502866+jowensoc@users.noreply.github.com>
2026-06-16 11:32:42 +0100

    DTR-5930: wip screen. used scaffolding
    # Conflicts:
    #	test-utils/generators/ModelGenerators.scala

---
+++ test/forms/IndividualContactMethodOptionsFormProviderSpec.scala
@@ -0,0 +1,29 @@
+package forms
+
+import forms.behaviours.CheckboxFieldBehaviours
+import models.IndividualContactMethodOptions
+import play.api.data.FormError
+
+class IndividualContactMethodOptionsFormProviderSpec extends CheckboxFieldBehaviours {
+
+  val form = new IndividualContactMethodOptionsFormProvider()()
+
+  ".value" - {
+
+    val fieldName = "value"
+    val requiredKey = "individualContactMethodOptions.error.required"
+
+    behave like checkboxField[IndividualContactMethodOptions](
+      form,
+      fieldName,
+      validValues  = IndividualContactMethodOptions.values,
+      invalidError = FormError(s"$fieldName[0]", "error.invalid")
+    )
+
+    behave like mandatoryCheckboxField(
+      form,
+      fieldName,
+      requiredKey
+    )
+  }
+}
