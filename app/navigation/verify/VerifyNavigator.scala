1b5b39badacdbbe145cf8627e3b3cfd24f5c3cf9
Juely Kaikade <254691220+Juely-Kaikade-HMRC@users.noreply.github.com>
2026-06-26 13:47:28 +0100

    DTR-4945-UPDATES - Routes & navigation updated

--- app/navigation/verify/VerifyNavigator.scala
+++ app/navigation/verify/VerifyNavigator.scala
@@ -67,7 +67,7 @@ class VerifyNavigator @Inject() () extends NavigatorForJourney {
     case SelectSubcontractorsToReverifyPage       =>
       userAnswers => navigatorFromSelectSubcontractorsToReverifyPage(CheckMode)(userAnswers)
     case EmailAddressPage                         =>
-      _ => controllers.verify.routes.VerificationDeclarationController.onPageLoad()
+      _ => controllers.verify.routes.VerifyCheckYourAnswersController.onPageLoad()
     case VerificationDeclarationPage              =>
       _ => controllers.verify.routes.VerifyCheckYourAnswersController.onPageLoad()
     case _                                        => _ => controllers.routes.JourneyRecoveryController.onPageLoad()
@@ -91,7 +91,7 @@ class VerifyNavigator @Inject() () extends NavigatorForJourney {
     mode match {
       case NormalMode => controllers.verify.routes.ReverifyExistingSubcontractorsYesNoController.onPageLoad(NormalMode)
       case CheckMode  =>
-        controllers.verify.routes.CheckVerificationBatchReadinessController.checkVerificationBatchReadiness()
+        controllers.verify.routes.CheckVerificationBatchReadinessController.checkVerificationBatchReadinessInCheckMode()
     }
   }
 
@@ -119,14 +119,20 @@ class VerifyNavigator @Inject() () extends NavigatorForJourney {
   private def navigatorFromContractorEmailConfirmationStoredPage(mode: Mode)(ua: UserAnswers): Call =
     (ua.get(ContractorEmailConfirmationStoredPage), mode) match {
 
-      case (Some(CurrentEmail), _) =>
+      case (Some(CurrentEmail), NormalMode) =>
         controllers.verify.routes.VerificationDeclarationController.onPageLoad()
+
+      case (Some(CurrentEmail), CheckMode) =>
+        controllers.verify.routes.VerifyCheckYourAnswersController.onPageLoad()
 
       case (Some(DifferentEmail), m) =>
         controllers.verify.routes.EmailAddressController.onPageLoad(m)
 
-      case (Some(DoNotSend), _) =>
+      case (Some(DoNotSend), NormalMode) =>
         controllers.verify.routes.VerificationDeclarationController.onPageLoad()
+
+      case (Some(DoNotSend), CheckMode) =>
+        controllers.verify.routes.VerifyCheckYourAnswersController.onPageLoad()
 
       case _ =>
         controllers.routes.JourneyRecoveryController.onPageLoad()
--- test/navigation/verify/VerifyNavigatorSpec.scala
+++ test/navigation/verify/VerifyNavigatorSpec.scala
@@ -206,7 +206,7 @@ class VerifyNavigatorSpec extends SpecBase {
 
         "must go to CheckVerificationBatchReadinessController in CheckMode" in {
           navigator.nextPage(SelectSubcontractorPage, CheckMode, emptyUserAnswers) mustBe
-            controllers.verify.routes.CheckVerificationBatchReadinessController.checkVerificationBatchReadiness()
+            controllers.verify.routes.CheckVerificationBatchReadinessController.checkVerificationBatchReadinessInCheckMode()
         }
       }
 
@@ -244,7 +244,7 @@ class VerifyNavigatorSpec extends SpecBase {
             ContractorEmailConfirmationStoredPage,
             CheckMode,
             ua
-          ) mustBe controllers.verify.routes.VerificationDeclarationController.onPageLoad()
+          ) mustBe cya
         }
 
         "must go to EmailAddressController in CheckMode when answer is DifferentEmail" in {
@@ -266,7 +266,7 @@ class VerifyNavigatorSpec extends SpecBase {
             ContractorEmailConfirmationStoredPage,
             CheckMode,
             ua
-          ) mustBe controllers.verify.routes.VerificationDeclarationController.onPageLoad()
+          ) mustBe cya
         }
 
         "must go to JourneyRecovery when answer is not present" in {
@@ -285,7 +285,7 @@ class VerifyNavigatorSpec extends SpecBase {
           EmailAddressPage,
           CheckMode,
           ua
-        ) mustBe controllers.verify.routes.VerificationDeclarationController.onPageLoad()
+        ) mustBe cya
       }
 
       "must go to VerifyCheckYourAnswers from VerificationDeclarationPage in CheckMode" in {
