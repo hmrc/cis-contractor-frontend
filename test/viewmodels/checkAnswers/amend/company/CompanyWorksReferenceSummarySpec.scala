588f86e4e71394f21ddd9a145055922e8703fb80
pvarsani <243989389+pvarsani@users.noreply.github.com>
2026-07-03 10:10:50 +0100

    Screen MSC-C10 [Amend Company] Works Reference Entry (AmendMode)

---
+++ test/viewmodels/checkAnswers/amend/company/CompanyWorksReferenceSummarySpec.scala
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels.checkAnswers.amend.company
+
+import controllers.add.company.routes
+import helpers.CyaEncodingSpecHelper
+import models.{AmendMode, UserAnswers}
+import org.scalatest.OptionValues.convertOptionToValuable
+import org.scalatest.TryValues.convertTryToSuccessOrFailure
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers.must
+import org.scalatest.matchers.should.Matchers
+import pages.add.company.CompanyWorksReferencePage
+import play.api.i18n.Messages
+import play.api.test.Helpers.stubMessages
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.*
+
+class CompanyWorksReferenceSummarySpec extends AnyFreeSpec with Matchers with CyaEncodingSpecHelper {
+  implicit val messages: Messages = stubMessages()
+
+  "AmendCompanyWorksReferenceSummary.row" - {
+
+    "must return a SummaryListRow when the answer exists" in {
+
+      val answers =
+        UserAnswers("test-id")
+          .set(CompanyWorksReferencePage, "WR-001")
+          .success
+          .value
+
+      val maybeRow = CompanyWorksReferenceSummary.row(answers)
+
+      maybeRow shouldBe defined
+
+      val row = maybeRow.value
+
+      val expectedKeyText = messages("companyWorksReference.checkYourAnswersLabel")
+      row.key.content.asHtml.toString should include(expectedKeyText)
+
+      row.value.content.asHtml.toString should include("WR-001")
+
+      row.actions shouldBe defined
+      val actions = row.actions.value.items
+      actions should have size 1
+
+      val changeAction       = actions.head
+      val expectedChangeText = messages("site.change")
+      val expectedHref       = routes.CompanyWorksReferenceController
+        .onPageLoad(AmendMode)
+        .url
+      val expectedHiddenText = messages("companyWorksReference.change.hidden")
+
+      changeAction.content.asHtml.toString    should include(expectedChangeText)
+      changeAction.href                     shouldBe expectedHref
+      changeAction.visuallyHiddenText.value shouldBe expectedHiddenText
+      changeAction.attributes                   must contain("id" -> "company-works-reference")
+    }
+
+    "must return None when the answer does not exist" in {
+
+      val answers = UserAnswers("test-id")
+
+      CompanyWorksReferenceSummary.row(answers) shouldBe None
+    }
+
+    "must HTML-escape special characters correctly (single encoding only)" in {
+
+      val worksRef = "WR-123 & Co 'Ref'"
+
+      val answers =
+        UserAnswers("id")
+          .set(CompanyWorksReferencePage, worksRef)
+          .success
+          .value
+
+      val row = CompanyWorksReferenceSummary.row(answers).value
+
+      val html = extractHtml(row)
+
+      assertEscaped(html, "WR-123 &amp; Co &#x27;Ref&#x27;")
+
+      assertNoDoubleEncoding(html)
+    }
+  }
+}
