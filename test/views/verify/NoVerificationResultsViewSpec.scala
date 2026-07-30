c473af1cbd7af47f3dc6ed3649f739c3cbb9c592
samraducan <231910514+samraducan@users.noreply.github.com>
2026-07-30 11:09:16 +0100

    DTR-6935: Add page content and unit tests

---
+++ test/views/verify/NoVerificationResultsViewSpec.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.verify
+
+import base.SpecBase
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import play.api.Application
+import play.api.i18n.Messages
+import play.api.test.FakeRequest
+import play.twirl.api.HtmlFormat
+import views.html.verify.NoVerificationResultsView
+
+class NoVerificationResultsViewSpec extends SpecBase {
+
+  "NoVerificationResultsView" - {
+    "render the page with title, heading, paragraph and link" in new Setup {
+      val html: HtmlFormat.Appendable = view()
+      val doc: Document = Jsoup.parse(html.body)
+
+      doc.select("title").text must include(messages("verify.noVerificationResults.title"))
+      doc.select("h1").text must include(messages("verify.noVerificationResults.heading"))
+      doc.select("p").text must include(messages("verify.noVerificationResults.p"))
+      doc.select("p").text must include(messages("verify.noVerificationResults.backTo"))
+      doc.select(".govuk-link").text must include(messages("verify.noVerificationResults.manageYourSubcontractors.link"))
+    }
+  }
+
+  trait Setup {
+    val app: Application = applicationBuilder().build()
+    val view: NoVerificationResultsView = app.injector.instanceOf[NoVerificationResultsView]
+    implicit val request: play.api.mvc.Request[_] = FakeRequest()
+    implicit val messages: Messages = play.api.i18n.MessagesImpl(
+      play.api.i18n.Lang.defaultLang,
+      app.injector.instanceOf[play.api.i18n.MessagesApi]
+    )
+  }
+}
