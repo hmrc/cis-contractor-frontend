6c5de7fd6dc4d41d7ae039e1f7d471765384e196
George Coldham <21260083+GeorgeColdham@users.noreply.github.com>
2026-08-18 13:48:29 +0100

    Fixed review comments

--- test/views/amend/AmendConfirmationViewSpec.scala
+++ test/views/amend/AmendConfirmationViewSpec.scala
@@ -108,7 +108,7 @@ class AmendConfirmationViewSpec extends AnyWordSpec with Matchers with GuiceOneA
 
       surveyLink.text() mustBe
         messages("amendConfirmation.beforeYouGo.takeAShortSurvey")
-      surveyLink.attr("href") mustBe appConfig.feedbackUrl
+      surveyLink.attr("rel") mustBe "noopener noreferrer"
     }
   }
 
