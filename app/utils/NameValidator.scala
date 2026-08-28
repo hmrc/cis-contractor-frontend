a35b2e54700a1d22133ef39bc8602935356c65d7
Jesse-Murray-Dev <209797357+Jesse-Murray-Dev@users.noreply.github.com>
2026-08-28 14:46:44 +0100

    scalafmt

--- app/utils/NameValidator.scala
+++ app/utils/NameValidator.scala
@@ -21,15 +21,15 @@ import models.validation.{FieldValidationFailure, SubcontractorValidationField}
 object NameValidator {
 
   def validate(
-                firstName: Option[String],
-                secondName: Option[String],
-                surname: Option[String],
-                tradingName: Option[String]
-              ): Seq[FieldValidationFailure] = {
+    firstName: Option[String],
+    secondName: Option[String],
+    surname: Option[String],
+    tradingName: Option[String]
+  ): Seq[FieldValidationFailure] = {
 
-    val firstNameBlank = firstName.forall(_.isBlank)
-    val secondNameBlank = secondName.forall(_.isBlank)
-    val surnameBlank = surname.forall(_.isBlank)
+    val firstNameBlank   = firstName.forall(_.isBlank)
+    val secondNameBlank  = secondName.forall(_.isBlank)
+    val surnameBlank     = surname.forall(_.isBlank)
     val tradingNameBlank = tradingName.forall(_.isBlank)
 
     val failures = Seq.newBuilder[FieldValidationFailure]
