5b3af28b96dfd4c1f7c072ad252ecadaf2324174
Sukesh Machanuru <35919670+sukesh90@users.noreply.github.com>
2026-09-09 09:29:54 +0100

    DTR-6897 UTR message change (#397)
    * DTR-6897 F31/F35 VF-07-03 review unmatched subcontractors
    
    * changes after minor update
    
    * added TODO
    
    * Changes after ScalafmtAll
    
    * changes after addressing review comments
    
    * commit after fixing test cases
    
    * DTR-6897 UTR message change

--- test/views/verify/ReviewUnmatchedSubcontractorsViewSpec.scala
+++ test/views/verify/ReviewUnmatchedSubcontractorsViewSpec.scala
@@ -95,7 +95,7 @@ class ReviewUnmatchedSubcontractorsViewSpec extends SpecBase {
       val table = document.getElementById("ready-table")
       table must not be null
 
-      table.select("thead th").text must include(messagesImpl("verify.reviewUnmatched.utr.full"))
+      table.select("thead th").text must include(messagesImpl("verify.reviewUnmatched.utr"))
       table.text                    must include("Smith, John")
       table.text                    must include("1234567890")
     }
