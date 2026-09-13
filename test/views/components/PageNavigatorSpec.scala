20f4fb3b88bdec667b1ba7e6b6f79ae861136f89
suryabalasingh <22298166+suryabalasingh@users.noreply.github.com>
2026-09-13 20:37:16 +0100

    DTR-6966-Reformatted code

--- test/views/components/PageNavigatorSpec.scala
+++ test/views/components/PageNavigatorSpec.scala
@@ -32,6 +32,7 @@ class PageNavigatorSpec extends SpecBase with Matchers {
     "must render nothing when there are no pagination items" in new Setup {
       val html = pageNavigator(PaginationViewModel(), page = 1)
       val doc  = Jsoup.parse(html.body)
+
       doc.select(".govuk-pagination").size() mustBe 0
     }
 
@@ -42,46 +43,150 @@ class PageNavigatorSpec extends SpecBase with Matchers {
           PaginationItemViewModel("2", "/test?page=2")
         )
       )
-      val doc        = parse(pageNavigator(pagination, page = 1))
+
+      val doc = parse(pageNavigator(pagination, page = 1))
+
       doc.select(".govuk-pagination").size() mustBe 1
       doc.select(".govuk-pagination__item").size() mustBe 2
     }
 
     "must render a previous button with value page - 1" in new Setup {
       val pagination = PaginationViewModel(
-        items = Seq(PaginationItemViewModel("2", "/test?page=2").withCurrent(true)),
+        items = Seq(
+          PaginationItemViewModel("2", "/test?page=2").withCurrent(true)
+        ),
         previous = Some(PaginationLinkViewModel("/test?page=1"))
       )
-      val doc        = parse(pageNavigator(pagination, page = 2))
-      val prevButton = doc.select(".govuk-pagination__prev button[name=gotoPage][value=1]")
+
+      val doc = parse(pageNavigator(pagination, page = 2))
+
+      val prevButton =
+        doc.select(".govuk-pagination__prev button[name=gotoPage][value=1]")
+
       prevButton.size() mustBe 1
-      prevButton.text() must include(messages("site.pagination.previous"))
+      prevButton.text() must include(
+        messages("site.pagination.previous")
+      )
+    }
+
+    "must render visually hidden text for the previous button" in new Setup {
+      val pagination = PaginationViewModel(
+        items = Seq(
+          PaginationItemViewModel("2", "/test?page=2").withCurrent(true)
+        ),
+        previous = Some(PaginationLinkViewModel("/test?page=1"))
+      )
+
+      val doc = parse(pageNavigator(pagination, page = 2))
+
+      val hiddenText =
+        doc.select(".govuk-pagination__prev .govuk-visually-hidden")
+
+      hiddenText.size() mustBe 1
+      hiddenText.text() mustBe
+        messages("site.pagination.goToPrevious")
+    }
+
+    "must render previous button with the correct attributes" in new Setup {
+      val pagination = PaginationViewModel(
+        items = Seq(
+          PaginationItemViewModel("2", "/test?page=2").withCurrent(true)
+        ),
+        previous = Some(PaginationLinkViewModel("/test?page=1"))
+      )
+
+      val doc = parse(pageNavigator(pagination, page = 2))
+
+      val prevButton =
+        doc.select(".govuk-pagination__prev button")
+
+      prevButton.size() mustBe 1
+      prevButton.attr("type") mustBe "submit"
+      prevButton.attr("name") mustBe "gotoPage"
+      prevButton.attr("value") mustBe "1"
+      prevButton.attr("rel") mustBe "prev"
+      prevButton.hasClass("govuk-pagination__link--button") mustBe true
     }
 
     "must render a next button with value page + 1" in new Setup {
       val pagination = PaginationViewModel(
-        items = Seq(PaginationItemViewModel("1", "/test?page=1").withCurrent(true)),
+        items = Seq(
+          PaginationItemViewModel("1", "/test?page=1").withCurrent(true)
+        ),
         next = Some(PaginationLinkViewModel("/test?page=2"))
       )
-      val doc        = parse(pageNavigator(pagination, page = 1))
-      val nextButton = doc.select(".govuk-pagination__next button[name=gotoPage][value=2]")
+
+      val doc = parse(pageNavigator(pagination, page = 1))
+
+      val nextButton =
+        doc.select(".govuk-pagination__next button[name=gotoPage][value=2]")
+
       nextButton.size() mustBe 1
-      nextButton.text() must include(messages("site.pagination.next"))
+      nextButton.text() must include(
+        messages("site.pagination.next")
+      )
+    }
+
+    "must render visually hidden text for the next button" in new Setup {
+      val pagination = PaginationViewModel(
+        items = Seq(
+          PaginationItemViewModel("1", "/test?page=1").withCurrent(true)
+        ),
+        next = Some(PaginationLinkViewModel("/test?page=2"))
+      )
+
+      val doc = parse(pageNavigator(pagination, page = 1))
+
+      val hiddenText =
+        doc.select(".govuk-pagination__next .govuk-visually-hidden")
+
+      hiddenText.size() mustBe 1
+      hiddenText.text() mustBe
+        messages("site.pagination.goToNext")
+    }
+
+    "must render next button with the correct attributes" in new Setup {
+      val pagination = PaginationViewModel(
+        items = Seq(
+          PaginationItemViewModel("1", "/test?page=1").withCurrent(true)
+        ),
+        next = Some(PaginationLinkViewModel("/test?page=2"))
+      )
+
+      val doc = parse(pageNavigator(pagination, page = 1))
+
+      val nextButton =
+        doc.select(".govuk-pagination__next button")
+
+      nextButton.size() mustBe 1
+      nextButton.attr("type") mustBe "submit"
+      nextButton.attr("name") mustBe "gotoPage"
+      nextButton.attr("value") mustBe "2"
+      nextButton.attr("rel") mustBe "next"
+      nextButton.hasClass("govuk-pagination__link--button") mustBe true
     }
 
     "must not render previous when not set" in new Setup {
       val pagination = PaginationViewModel(
-        items = Seq(PaginationItemViewModel("1", "/test?page=1").withCurrent(true))
+        items = Seq(
+          PaginationItemViewModel("1", "/test?page=1").withCurrent(true)
+        )
       )
-      val doc        = parse(pageNavigator(pagination, page = 1))
+
+      val doc = parse(pageNavigator(pagination, page = 1))
+
       doc.select(".govuk-pagination__prev").size() mustBe 0
     }
 
     "must not render next when not set" in new Setup {
       val pagination = PaginationViewModel(
-        items = Seq(PaginationItemViewModel("1", "/test?page=1").withCurrent(true))
+        items = Seq(
+          PaginationItemViewModel("1", "/test?page=1").withCurrent(true)
+        )
       )
-      val doc        = parse(pageNavigator(pagination, page = 1))
+
+      val doc = parse(pageNavigator(pagination, page = 1))
+
       doc.select(".govuk-pagination__next").size() mustBe 0
     }
 
@@ -93,9 +198,11 @@ class PageNavigatorSpec extends SpecBase with Matchers {
           PaginationItemViewModel("3", "/test?page=3")
         )
       )
-      val doc        = parse(pageNavigator(pagination, page = 2))
+
+      val doc = parse(pageNavigator(pagination, page = 2))
+
       doc.select(".govuk-pagination__item--current").size() mustBe 1
-      doc.select(".govuk-pagination__item--current [aria-current=page]").size() mustBe 1
+      doc.select(".govuk-pagination__item--current button[aria-current=page]").size() mustBe 1
     }
 
     "must render page number buttons with correct gotoPage values" in new Setup {
@@ -106,28 +213,127 @@ class PageNavigatorSpec extends SpecBase with Matchers {
           PaginationItemViewModel("3", "/test?page=3")
         )
       )
-      val doc        = parse(pageNavigator(pagination, page = 1))
+
+      val doc = parse(pageNavigator(pagination, page = 1))
+
       doc.select(".govuk-pagination__item button[name=gotoPage][value=1]").size() mustBe 1
       doc.select(".govuk-pagination__item button[name=gotoPage][value=2]").size() mustBe 1
       doc.select(".govuk-pagination__item button[name=gotoPage][value=3]").size() mustBe 1
     }
 
+    "must render the correct aria-label for each page number button" in new Setup {
+      val pagination = PaginationViewModel(
+        items = Seq(
+          PaginationItemViewModel("1", "/test?page=1"),
+          PaginationItemViewModel("2", "/test?page=2"),
+          PaginationItemViewModel("3", "/test?page=3")
+        )
+      )
+
+      val doc = parse(pageNavigator(pagination, page = 1))
+
+      val buttons =
+        doc.select(".govuk-pagination__item button")
+
+      buttons.size() mustBe 3
+
+      buttons.get(0).attr("aria-label") mustBe
+        messages("site.pagination.goToPage", "1")
+
+      buttons.get(1).attr("aria-label") mustBe
+        messages("site.pagination.goToPage", "2")
+
+      buttons.get(2).attr("aria-label") mustBe
+        messages("site.pagination.goToPage", "3")
+    }
+
+    "must render the page number as the button text" in new Setup {
+      val pagination = PaginationViewModel(
+        items = Seq(
+          PaginationItemViewModel("1", "/test?page=1"),
+          PaginationItemViewModel("2", "/test?page=2"),
+          PaginationItemViewModel("3", "/test?page=3")
+        )
+      )
+
+      val doc = parse(pageNavigator(pagination, page = 1))
+
+      val buttons =
+        doc.select(".govuk-pagination__item button")
+
+      buttons.get(0).text() mustBe "1"
+      buttons.get(1).text() mustBe "2"
+      buttons.get(2).text() mustBe "3"
+    }
+
     "must use the landmark label from the view model" in new Setup {
       val pagination = PaginationViewModel(
-        items = Seq(PaginationItemViewModel("1", "/test?page=1")),
+        items = Seq(
+          PaginationItemViewModel("1", "/test?page=1")
+        ),
         landmarkLabel = "site.pagination.landmark"
       )
-      val doc        = parse(pageNavigator(pagination, page = 1))
-      doc.select(".govuk-pagination").attr("aria-label") mustBe messages("site.pagination.landmark")
+
+      val doc = parse(pageNavigator(pagination, page = 1))
+
+      doc.select(".govuk-pagination").attr("aria-label") mustBe
+        messages("site.pagination.landmark")
+    }
+
+    "must render previous and next buttons together when both are present" in new Setup {
+      val pagination = PaginationViewModel(
+        items = Seq(
+          PaginationItemViewModel("2", "/test?page=2").withCurrent(true)
+        ),
+        previous = Some(PaginationLinkViewModel("/test?page=1")),
+        next = Some(PaginationLinkViewModel("/test?page=3"))
+      )
+
+      val doc = parse(pageNavigator(pagination, page = 2))
+
+      doc.select(".govuk-pagination__prev button").size() mustBe 1
+      doc.select(".govuk-pagination__next button").size() mustBe 1
+
+      doc.select(".govuk-pagination__prev button").attr("value") mustBe "1"
+      doc.select(".govuk-pagination__next button").attr("value") mustBe "3"
+    }
+
+    "must render pagination icons as hidden from assistive technology" in new Setup {
+      val pagination = PaginationViewModel(
+        items = Seq(
+          PaginationItemViewModel("2", "/test?page=2").withCurrent(true)
+        ),
+        previous = Some(PaginationLinkViewModel("/test?page=1")),
+        next = Some(PaginationLinkViewModel("/test?page=3"))
+      )
+
+      val doc = parse(pageNavigator(pagination, page = 2))
+
+      val icons = doc.select(".govuk-pagination__icon")
+
+      icons.size() mustBe 2
+
+      icons.get(0).attr("aria-hidden") mustBe "true"
+      icons.get(0).attr("focusable") mustBe "false"
+
+      icons.get(1).attr("aria-hidden") mustBe "true"
+      icons.get(1).attr("focusable") mustBe "false"
     }
   }
 
   trait Setup {
-    val app                                                                = applicationBuilder().build()
-    val pageNavigator: PageNavigator                                       = app.injector.instanceOf[PageNavigator]
-    implicit val request: FakeRequest[play.api.mvc.AnyContentAsEmpty.type] = FakeRequest()
-    implicit val messages: Messages                                        = app.injector.instanceOf[play.api.i18n.MessagesApi].preferred(request)
+    val app           = applicationBuilder().build()
+    val pageNavigator = app.injector.instanceOf[PageNavigator]
 
-    def parse(html: play.twirl.api.Html): Document = Jsoup.parse(html.body)
+    implicit val request: FakeRequest[play.api.mvc.AnyContentAsEmpty.type] =
+      FakeRequest()
+
+    implicit val messages: Messages =
+      app.injector
+        .instanceOf[play.api.i18n.MessagesApi]
+        .preferred(request)
+
+    def parse(html: play.twirl.api.Html): Document =
+      Jsoup.parse(html.body)
   }
 }
--- test/views/verify/ReviewUnmatchedSubcontractorsViewSpec.scala
+++ test/views/verify/ReviewUnmatchedSubcontractorsViewSpec.scala
@@ -27,11 +27,13 @@ import views.html.verify.ReviewUnmatchedSubcontractorsView
 
 class ReviewUnmatchedSubcontractorsViewSpec extends SpecBase {
 
-  private implicit val request: Request[?]    = FakeRequest()
+  private implicit val request: Request[?] = FakeRequest()
+
   private implicit val messagesImpl: Messages =
     app.injector.instanceOf[play.api.i18n.MessagesApi].preferred(FakeRequest())
 
-  private val view = app.injector.instanceOf[ReviewUnmatchedSubcontractorsView]
+  private val view =
+    app.injector.instanceOf[ReviewUnmatchedSubcontractorsView]
 
   private def link(name: String) = LinkViewModel("#", name)
 
@@ -58,7 +60,8 @@ class ReviewUnmatchedSubcontractorsViewSpec extends SpecBase {
   "ReviewUnmatchedSubcontractorsView" - {
 
     "must render the heading, title and introductory content" in {
-      val document = doc(ReviewUnmatchedViewModel(unmatched = Seq(unmatchedRow), ready = Nil))
+      val document =
+        doc(ReviewUnmatchedViewModel(unmatched = Seq(unmatchedRow), ready = Nil))
 
       document.title     must include(messagesImpl("verify.reviewUnmatched.title"))
       document.select("h1").text mustBe messagesImpl("verify.reviewUnmatched.heading")
@@ -68,20 +71,26 @@ class ReviewUnmatchedSubcontractorsViewSpec extends SpecBase {
     }
 
     "must render the guidance link opening in a new tab" in {
-      val document = doc(ReviewUnmatchedViewModel(unmatched = Seq(unmatchedRow), ready = Nil))
+      val document =
+        doc(ReviewUnmatchedViewModel(unmatched = Seq(unmatchedRow), ready = Nil))
 
-      val whatYouNeed = document.select("a[href$='what-you-must-do-as-a-cis-contractor']")
+      val whatYouNeed =
+        document.select("a[href$='what-you-must-do-as-a-cis-contractor']")
+
       whatYouNeed.size() mustBe 1
       whatYouNeed.attr("target") mustBe "_blank"
-      whatYouNeed.text must include(messagesImpl("verify.reviewUnmatched.whatYouNeed.link"))
+      whatYouNeed.text must include(
+        messagesImpl("verify.reviewUnmatched.whatYouNeed.link")
+      )
     }
 
     "must render the unmatched subcontractors table with Edit, Proceed and Remove actions" in {
-      val document = doc(ReviewUnmatchedViewModel(unmatched = Seq(unmatchedRow), ready = Nil))
+      val document =
+        doc(ReviewUnmatchedViewModel(unmatched = Seq(unmatchedRow), ready = Nil))
 
       val table = document.getElementById("unmatched-subcontractors-table")
-      table must not be null
 
+      table      must not be null
       table.text must include("Brody, Martin")
       table.text must include(messagesImpl("verify.reviewUnmatched.noneProvided"))
       table.text must include(messagesImpl("verify.reviewUnmatched.action.edit"))
@@ -89,41 +98,188 @@ class ReviewUnmatchedSubcontractorsViewSpec extends SpecBase {
       table.text must include(messagesImpl("verify.reviewUnmatched.action.remove"))
     }
 
+    "must render the subcontractor name link with visually hidden text" in {
+      val document =
+        doc(ReviewUnmatchedViewModel(unmatched = Seq(unmatchedRow), ready = Nil))
+
+      val nameLink =
+        document.select("#unmatched-subcontractors-table a").first()
+
+      nameLink.text mustBe
+        s"Brody, Martin ${messagesImpl("verify.reviewUnmatched.name.hidden", "Brody, Martin")}"
+
+      val hiddenText =
+        nameLink.select(".govuk-visually-hidden")
+
+      hiddenText.size() mustBe 1
+      hiddenText.text mustBe
+        messagesImpl("verify.reviewUnmatched.name.hidden", "Brody, Martin")
+    }
+
+    "must render visually hidden text for each unmatched action link" in {
+      val document =
+        doc(ReviewUnmatchedViewModel(unmatched = Seq(unmatchedRow), ready = Nil))
+
+      val actionLinks =
+        document.select("#unmatched-subcontractors-table a")
+
+      actionLinks.size() mustBe 4
+
+      actionLinks.get(1).text mustBe
+        s"${messagesImpl("verify.reviewUnmatched.action.edit")} " +
+        messagesImpl(
+          "verify.reviewUnmatched.action.edit.hidden",
+          "Brody, Martin"
+        )
+
+      actionLinks.get(2).text mustBe
+        s"${messagesImpl("verify.reviewUnmatched.action.proceed")} " +
+        messagesImpl(
+          "verify.reviewUnmatched.action.proceed.hidden",
+          "Brody, Martin"
+        )
+
+      actionLinks.get(3).text mustBe
+        s"${messagesImpl("verify.reviewUnmatched.action.remove")} " +
+        messagesImpl(
+          "verify.reviewUnmatched.action.remove.hidden",
+          "Brody, Martin"
+        )
+    }
+
+    "must render the correct visually hidden text inside each unmatched action link" in {
+      val document =
+        doc(ReviewUnmatchedViewModel(unmatched = Seq(unmatchedRow), ready = Nil))
+
+      val actionLinks =
+        document.select("#unmatched-subcontractors-table a")
+
+      actionLinks.size() mustBe 4
+
+      actionLinks
+        .get(1)
+        .select(".govuk-visually-hidden")
+        .text mustBe
+        messagesImpl(
+          "verify.reviewUnmatched.action.edit.hidden",
+          "Brody, Martin"
+        )
+
+      actionLinks
+        .get(2)
+        .select(".govuk-visually-hidden")
+        .text mustBe
+        messagesImpl(
+          "verify.reviewUnmatched.action.proceed.hidden",
+          "Brody, Martin"
+        )
+
+      actionLinks
+        .get(3)
+        .select(".govuk-visually-hidden")
+        .text mustBe
+        messagesImpl(
+          "verify.reviewUnmatched.action.remove.hidden",
+          "Brody, Martin"
+        )
+    }
+
+    "must render separators between unmatched action links as aria-hidden" in {
+      val document =
+        doc(ReviewUnmatchedViewModel(unmatched = Seq(unmatchedRow), ready = Nil))
+
+      val separators =
+        document.select("#unmatched-subcontractors-table span[aria-hidden='true']")
+
+      separators.size() mustBe 2
+      separators.eachText() mustBe java.util.Arrays.asList("|", "|")
+    }
+
     "must render the ready table with the full Unique Taxpayer Reference header" in {
-      val document = doc(ReviewUnmatchedViewModel(unmatched = Nil, ready = Seq(readyRow)))
+      val document =
+        doc(ReviewUnmatchedViewModel(unmatched = Nil, ready = Seq(readyRow)))
 
       val table = document.getElementById("ready-table")
-      table must not be null
 
-      table.select("thead th").text must include(messagesImpl("verify.reviewUnmatched.utr"))
+      table                         must not be null
+      table.select("thead th").text must include(
+        messagesImpl("verify.reviewUnmatched.utr")
+      )
       table.text                    must include("Smith, John")
       table.text                    must include("1234567890")
     }
 
+    "must render visually hidden text for a ready subcontractor name" in {
+      val document =
+        doc(ReviewUnmatchedViewModel(unmatched = Nil, ready = Seq(readyRow)))
+
+      val nameLink =
+        document.select("#ready-table a").first()
+
+      nameLink.text mustBe
+        s"Smith, John ${messagesImpl("verify.reviewUnmatched.name.hidden", "Smith, John")}"
+
+      nameLink
+        .select(".govuk-visually-hidden")
+        .text mustBe
+        messagesImpl("verify.reviewUnmatched.name.hidden", "Smith, John")
+    }
+
     "must not render the unmatched table when there are no unmatched subcontractors" in {
-      val document = doc(ReviewUnmatchedViewModel(unmatched = Nil, ready = Seq(readyRow)))
+      val document =
+        doc(ReviewUnmatchedViewModel(unmatched = Nil, ready = Seq(readyRow)))
 
       document.getElementById("unmatched-subcontractors-table") mustBe null
     }
 
     "must not render the ready table when there are no ready subcontractors" in {
-      val document = doc(ReviewUnmatchedViewModel(unmatched = Seq(unmatchedRow), ready = Nil))
+      val document =
+        doc(ReviewUnmatchedViewModel(unmatched = Seq(unmatchedRow), ready = Nil))
 
       document.getElementById("ready-table") mustBe null
     }
 
     "must render a Continue button only when all subcontractors are ready" in {
-      val allReady = doc(ReviewUnmatchedViewModel(unmatched = Nil, ready = Seq(readyRow)))
-      allReady.select(".govuk-button").text must include(messagesImpl("site.continue"))
+      val allReady =
+        doc(ReviewUnmatchedViewModel(unmatched = Nil, ready = Seq(readyRow)))
 
-      val stillUnmatched = doc(ReviewUnmatchedViewModel(unmatched = Seq(unmatchedRow), ready = Seq(readyRow)))
+      allReady.select(".govuk-button").text must include(
+        messagesImpl("site.continue")
+      )
+
+      val stillUnmatched =
+        doc(
+          ReviewUnmatchedViewModel(
+            unmatched = Seq(unmatchedRow),
+            ready = Seq(readyRow)
+          )
+        )
+
       stillUnmatched.select(".govuk-button").size() mustBe 0
     }
 
     "must render the 'Back to Verification results' link" in {
-      val document = doc(ReviewUnmatchedViewModel(unmatched = Seq(unmatchedRow), ready = Nil))
+      val document =
+        doc(ReviewUnmatchedViewModel(unmatched = Seq(unmatchedRow), ready = Nil))
 
-      document.body.text must include(messagesImpl("verify.reviewUnmatched.backToResults.link"))
+      document.body.text must include(
+        messagesImpl("verify.reviewUnmatched.backToResults.link")
+      )
+    }
+
+    "must render the back to results link with the correct prefix and full stop" in {
+      val document =
+        doc(ReviewUnmatchedViewModel(unmatched = Seq(unmatchedRow), ready = Nil))
+
+      val backToResults =
+        document.select(
+          s"a[href='${controllers.verify.routes.VerificationResultsController.onPageLoad().url}']"
+        )
+
+      backToResults.size() mustBe 1
+      backToResults.text mustBe
+        s"${messagesImpl("verify.reviewUnmatched.backToResults.prefix")} " +
+        s"${messagesImpl("verify.reviewUnmatched.backToResults.link")}"
     }
   }
 }
